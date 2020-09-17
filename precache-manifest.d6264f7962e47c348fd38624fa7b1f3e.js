86151ac58c1c45f01b49b0e772b0c6ceb7592c11
thanhvinhhumg <thanhvinh200293@gmail.com>
2020-09-17 15:36:18 +0700

    Updates

---
+++ precache-manifest.d6264f7962e47c348fd38624fa7b1f3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b72bef980c178cfcb29ceaa35111a9",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "38ff37180fdc68bec50d",
+    "url": "/monster-rolodex/static/css/main.53b63f60.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ff37180fdc68bec50d",
+    "url": "/monster-rolodex/static/js/main.eb264fda.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
